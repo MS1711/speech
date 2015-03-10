4f458806ff80dfd5bb3cad3b34159c9dc5c15247
wangtt03 <wangtt03@163.com>
2015-03-10 12:25:56 +0800

    add device simulator

---
+++ DeviceSim/public/js/index.js
@@ -0,0 +1,160 @@
+﻿// attach ready event
+function lightAON(){
+            $("#lightA").show();
+        }
+function lightAOFF(){
+            $("#lightA").hide();
+        }
+function lightBON(){
+            $("#lightB").show();
+        }
+function lightBOFF(){
+            $("#lightB").hide();
+        }
+function curtainOPEN(){
+            $("#curtain").hide();
+        }
+
+function curtainCLOSE(){
+            $("#curtain").show();
+        }
+function TVON(){
+            $("#TV").get(0).play();
+}
+
+function TVOFF(){
+            $("#TV").get(0).pause();
+}
+
+var lastaction ="";
+var lastobject ="";
+var lasturl="";
+var lastverbose  = "";
+function askForRenew(){
+    $.ajax({
+        url:'/renewpage',
+        type:'POST',
+        dataType:'json',
+        data:{},
+        success:function(result){
+            var action = result.object;
+            var object = result.action;
+            var url = result.url;
+            var verbose = result.verbose;
+            if (lastaction == action && lastobject == object && lasturl == url && lastverbose == verbose)
+                return;
+            lastaction = action;
+            lastobject = object;
+            lasturl = url;
+            lastverbose = verbose;
+            console.log(result);
+			
+			document.getElementById("mediaDiv").style.display = "none";
+			MediaPlayer.URL = "";
+			MediaPlayer.controls.stop();
+			document.getElementById("audioplayer").src = "";
+			document.getElementById("hintmessage").style.top="500px";
+			
+					
+            if (object == "MUSIC" || object == "RADIO"){
+                document.getElementById("airmessage").style.display="none";
+                if (action == "START"){
+                    //clear other audio
+                    document.getElementById("radioframe").setAttribute("src","");
+
+                    document.getElementById("audioplayer").setAttribute("src",url);
+                    document.getElementById("audioplayer").play();
+                    document.getElementById("hintmessage").style.display="";
+                    if (object == "RADIO"){
+						 document.getElementById("nowplaying").innerHTML = "" + verbose;
+						 document.getElementById("mediaDiv").style.display = "";
+						 document.getElementById("hintmessage").style.top="300px";
+						 //document.getElementById("MediaPlayer").URL = url;
+						 MediaPlayer.URL = url;
+						 MediaPlayer.controls.play();
+					}
+                       
+                    else
+                        document.getElementById("nowplaying").innerHTML = verbose;
+                    document.getElementById("nowplaying").setAttribute("href",url);
+
+                }else if (action == "STOP"){
+                    document.getElementById("audioplayer").src = "";
+                    document.getElementById("hintmessage").style.display="none";
+					document.getElementById("mediaDiv").style.display = "none";
+					MediaPlayer.URL = "";
+					MediaPlayer.controls.play();
+                }
+            }else if (object == "STORY"){
+                if (action == "START"){
+                    document.getElementById("audioplayer").setAttribute("src","");
+
+                    document.getElementById("radioframe").setAttribute("src",url);
+                    document.getElementById("hintmessage").style.display="";
+                    document.getElementById("nowplaying").innerHTML = verbose;
+                    document.getElementById("nowplaying").setAttribute("href",url);
+
+                }else if (action == "STOP"){
+                    document.getElementById("radioframe").setAttribute("src","");
+                    document.getElementById("hintmessage").style.display="none";
+                }
+            }
+            else if (object == "LIGHT"){
+                if (action == "START"){
+                    if (url == "客厅")
+                        lightAON();
+                    else if (url == "餐厅")
+                        lightBON();
+                    else {
+                        lightAON();
+                        lightBON();
+                    }
+                }
+                else {
+                    if (url == '客厅')
+                        lightAOFF();
+                    else if (url == "餐厅")
+                        lightBOFF();
+                    else {
+                        lightAOFF();
+                        lightBOFF();
+                    }
+                }
+            }else if (object == "CURTAIN"){
+                if (action == "START")
+                    curtainOPEN();
+                else
+                    curtainCLOSE();
+            }else if (object == "AIR"){
+                document.getElementById("hintmessage").style.display="none";
+                document.getElementById("airmessage").style.display="";
+                if (action == "START"){
+                    document.getElementById("airinfo").innerHTML = "空调已打开";
+                }else if (action == "STOP"){
+                    document.getElementById("airinfo").innerHTML = "空调已关闭";
+                }if (action == "UP"){
+                    document.getElementById("airinfo").innerHTML = "空调温度已经调高" + verbose;
+                }if (action == "DOWN"){
+                    document.getElementById("airinfo").innerHTML = "空调温度已经调低" + verbose;
+                }if (action == "CHANGE"){
+                    document.getElementById("airinfo").innerHTML = "空调温度已经设置为" + verbose;
+                }
+            }else if (object == "X"){
+                document.getElementById("hintmessage").style.display ="none";
+                document.getElementById("audioplayer").setAttribute("src","");
+                document.getElementById("radioframe").setAttribute("src","");
+
+            }
+        }
+    });
+}
+
+
+$(function(){
+  $("#lightA").hide();
+  $("#lightB").hide();
+  $("#curtain").hide();
+    setInterval("askForRenew()",500);
+});
+
+
---
+++ DeviceSim/routes/renewpage.js
@@ -0,0 +1,48 @@
+﻿
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+router.post('/',function (req, res){
+
+    var commands = [];
+    var linenum = 0;
+    fs.readFile('C:/workspace/newTemp.txt','utf-8',function(err,data){
+        if(err){
+            console.error(err);
+        }
+        else{
+            commands = data.split('\n');
+            for (var i = 0; i < commands.length; i++){
+                if (commands[i][0] != 'X'){
+                    linenum = i;
+                    action = commands[i].split('$');
+                    res.json({'object':action[0],'action':action[1],'url':action[2],'verbose':action[3]});
+                    //console.log(res);
+                    break;
+                }
+            }
+        }
+    });
+    /*
+    var towrite = "";
+    for (var i = 0; i < commands.length; i++){
+        if (i != linenum){
+            towrite += commands[i];
+            towrite += '\n';
+        }else{
+            towrite += "X";
+            towrite += commands[i];
+            towrite += '\n';
+        }
+    }
+
+    fs.writeFile('E:/newTemp.txt', towrite, function(err){
+        if(err) throw err;
+        console.log('内容被覆盖');
+    });
+*/
+
+
+});
+
+module.exports = router;
